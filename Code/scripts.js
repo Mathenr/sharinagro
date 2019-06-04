8976b8dd947fdcc75d7e250a16331f71292802f3
Mateus <31441471+Mathenr@users.noreply.github.com>
2019-06-04 02:46:24 -0300

    Add files via upload

---
+++ Code/scripts.js
@@ -0,0 +1,20 @@
+$('#s1_navicon').click(function (e) { 
+  $('#s1_nav').animate({
+    left: "+=100%"
+  }, 300, function() {});
+
+  $('body').css('overflow', 'hidden');
+  e.preventDefault();
+
+});
+
+$('#s1_naviconclose').click(function (e) { 
+  $('#s1_nav').animate({
+    left: "-=100%"
+  }, 300, function() {});
+
+  $('body').css('overflow', 'visible');
+  e.preventDefault();
+
+});
+
